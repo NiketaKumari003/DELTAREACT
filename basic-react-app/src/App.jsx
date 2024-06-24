550197d0f0e1d1e006c7614ce18ae0154ddda529
delta-demo <niketa78709846@gmail.com>
2024-06-24 17:22:22 +0530

    flies added

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import"./App.css";
+import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
+//import 
+
+function App(){
+  return (
+<ProductTab/>
+  );
+
+  
+}
+export default App;
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,10 @@
+function Title(){
+    let name="shradha";
+    return (
+        <div>
+            <p>2*2={2*2}</p>
+            <p>Hi,{name.toUpperCase()}</p>
+        </div>
+    );
+}
+export default Title;
